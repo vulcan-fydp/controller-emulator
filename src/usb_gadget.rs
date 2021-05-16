f926dac1db34cdff6b00d2734b693c3710d2aa01
Callum Moseley <callum.moseley1@gmail.com>
2021-05-16 15:00:20 -0500

    fix

--- src/usb_gadget.rs
+++ src/usb_gadget.rs
@@ -144,9 +144,12 @@ impl Gadget {
 
 pub fn activate(name: &str) -> Result<()> {
     let path = Path::new("/sys/kernel/config/usb_gadget")
-            .join(name)
-            .join("UDC");
-    Command::new("sh").arg("-c").arg(format!("echo \"\" > {}", path.display())).status()?;
+        .join(name)
+        .join("UDC");
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("echo \"\" > {}", path.display()))
+        .status()?;
     let output = Command::new("ls").arg("/sys/class/udc").output()?;
     let mut udc = File::create(&path)?;
     udc.write_all(&output.stdout)?;

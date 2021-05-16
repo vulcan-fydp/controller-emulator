5bfde8325938297b887faf8e2f28930aed92eaa2
Callum Moseley <callum.moseley1@gmail.com>
2021-05-16 13:52:37 -0500

    testing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,6 +2,8 @@ use controller_emulator::controller::ns_procon;
 use controller_emulator::controller::Controller;
 use controller_emulator::usb_gadget;
 use controller_emulator::usb_gadget::ns_procon::ns_procons;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let procons = ns_procons();
@@ -16,9 +18,19 @@ fn main() {
         .start_comms()
         .expect("Couldn't start communicating");
 
-    procon_1.press(ns_procon::inputs::BUTTON_A);
-    procon_1.set_axis(ns_procon::inputs::AXIS_LH, 1 << 15);
-    procon_1.release(ns_procon::inputs::BUTTON_A);
+    sleep(Duration::from_secs(10));
+    // procon_1.set_axis(ns_procon::inputs::AXIS_LH, 1 << 15);
+    // procon_1.set_axis(ns_procon::inputs::AXIS_LV, 1 << 15);
+    // procon_1.set_axis(ns_procon::inputs::AXIS_RH, 1 << 15);
+    // procon_1.set_axis(ns_procon::inputs::AXIS_RV, 1 << 15);
+
+    // for i in 0..100 {
+    //     procon_1.press(ns_procon::inputs::BUTTON_A);
+    //     println!("{:?}", procon_1);
+    //     sleep(Duration::from_secs(1));
+    //     procon_1.release(ns_procon::inputs::BUTTON_A);
+    //     sleep(Duration::from_secs(1));
+    // }
 
     procon_1.stop();
 }
